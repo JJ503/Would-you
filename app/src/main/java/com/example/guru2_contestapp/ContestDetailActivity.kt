2ae20ff550d8a25b13c01c7f2270b02ce02d7637
JJ503 <63184334+JJ503@users.noreply.github.com>
2021-02-05 04:41:31 +0900

    login, sign up, main v2

--- app/src/main/java/com/example/guru2_contestapp/ContestDetailActivity.kt
+++ app/src/main/java/com/example/guru2_contestapp/ContestDetailActivity.kt
@@ -3,13 +3,16 @@ package com.example.guru2_contestapp
 import android.content.Intent
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import android.graphics.Color
+import android.graphics.ColorFilter
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import android.view.View
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 
 class ContestDetailActivity : AppCompatActivity() {
@@ -25,8 +28,7 @@ class ContestDetailActivity : AppCompatActivity() {
     lateinit var end: TextView
     lateinit var detail: TextView
     lateinit var homepage: TextView
-    lateinit var wishOnBtn: ImageButton
-    lateinit var wishOffBtn: ImageButton
+    lateinit var wishBtn: ImageButton
 
     lateinit var str_name: String
     lateinit var str_host: String
@@ -49,8 +51,7 @@ class ContestDetailActivity : AppCompatActivity() {
         detail=findViewById(R.id.WdetailTextView)
         homepage=findViewById(R.id.WcontestLinkTextView)
         teamAddBtn=findViewById(R.id.WsearchTeamButton)
-        wishOnBtn=findViewById(R.id.wishOnButton)
-        wishOffBtn=findViewById(R.id.wishOffButton)
+        wishBtn=findViewById(R.id.wishButton)
 
         // 팀 목록에서 팀을 선택하면 그 팀이 참가하는 공모전 번호가 intent로 넘어온다.
         // 그 값으로 DB에서 공모전 이름, 주최기관 등 자세한 정보를 가져온다.
@@ -99,14 +100,17 @@ class ContestDetailActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-        wishOnBtn.setOnClickListener {
-            wishOnBtn.visibility= View.INVISIBLE
-            wishOffBtn.visibility= View.VISIBLE
-        }
-
-        wishOffBtn.setOnClickListener {
-            wishOnBtn.visibility= View.VISIBLE
-            wishOffBtn.visibility= View.INVISIBLE
+        wishBtn.setOnClickListener {
+            when(colorSwitch){
+                0 -> { //현재 white
+                    wishBtn.setTint(ContextCompat.getColor(this, R.color.impend))
+                    colorSwitch=1
+                }
+                1 -> { //현재 yellow
+                    wishBtn.setBackgroundColor(ContextCompat.getColor(this, R.color.normal))
+                    colorSwitch=0
+                }
+            }
         }
     }
 
