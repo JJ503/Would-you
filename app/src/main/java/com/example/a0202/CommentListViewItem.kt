59dea37fd12267a60f32b862d1b14bb172de8d5e
woogy4231 <61015431+woogy4231@users.noreply.github.com>
2021-02-05 00:36:36 +0900

    del

--- app/src/main/java/com/example/a0202/CommentListViewItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.a0202
-
-class CommentListViewItem (
-    val img: String, val name: String,
-    val comment: String, val date: String
-)
--- app/src/main/java/com/example/a0202/TeamListViewItem.kt
+++
@@ -1,6 +0,0 @@
-package com.example.a0202
-
-class TeamListViewItem (
-    val teamNum: Int, val img: String,
-    val teamName: String, val contestNum: String, val endDate: String,
-    val needPart: String, val totalNum: Int, val nowNum:Int)
