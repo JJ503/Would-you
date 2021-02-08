9b4c1e988b09ddd1d1a35b059ddc425a91c2501b
JJ503 <63184334+JJ503@users.noreply.github.com>
2021-02-08 16:57:10 +0900

    DBManager 수정

--- app/src/main/java/com/example/guru2_contestapp/DBManager.kt
+++ app/src/main/java/com/example/guru2_contestapp/DBManager.kt
@@ -10,45 +10,96 @@ class DBManager(
     factory: SQLiteDatabase.CursorFactory?,
     version: Int
 ): SQLiteOpenHelper(context, name, factory, version) {
+
     override fun onCreate(db: SQLiteDatabase?) {
-        db!!.execSQL("CREATE TABLE IF NOT EXISTS contest(c_num INTEGER, c_name TEXT, c_photo TEXT, c_host TEXT, c_section TEXT, c_start TEXT, c_end TEXT, c_address TEXT, c_detail TEXT, PRIMARY KEY(c_num))")
-        db!!.execSQL("CREATE TABLE IF NOT EXISTS team (t_num INTEGER, c_num INTEGER, t_name TEXT, t_total_num INTEGER, t_now_num INTEGER, t_endDate TEXT, t_need_part TEXT, t_detail TEXT, PRIMARY KEY(t_num))")
-        db!!.execSQL("CREATE TABLE IF NOT EXISTS comment(cm_idx INTEGER, t_num INTEGER, s_id TEXT, cm_date TEXT, cm_detail TEXT, PRIMARY KEY(cm_idx))")
-        db!!.execSQL("CREATE TABLE IF NOT EXISTS resume (r_idx INTEGER, s_id TEXT, t_num INTEGER, r_hope TEXT, r_self_intro TEXT, r_etc TEXT, PRIMARY KEY(r_idx))")
-
-        db!!.execSQL("CREATE TABLE teamManage (" +
-                "id TEXT NOT NULL," +
-                "t_num INTEGER," +
-                "state INTEGER NOT NULL," +
-                "FOREIGN KEY(id) REFERENCES member(id) ON DELETE CASCADE," +
-                "FOREIGN KEY(t_num) REFERENCES team(t_num) ON UPDATE CASCADE" +
+        // comment 테이블
+        db!!.execSQL("CREATE TABLE IF NOT EXISTS comment(" +
+                "cm_idx INTEGER NOT NULL," +
+                "t_num INTEGER NOT NULL," +
+                "m_id TEXT NOT NULL," +
+                "cm_date TEXT NOT NULL," +
+                "cm_detail TEXT NOT NULL," +
+                "PRIMARY KEY(cm_idx AUTOINCREMENT)" +
                 ");")
 
-        db!!.execSQL("CREATE TABLE wishlist (" +
-                "id TEXT NOT NULL," +
-                "t_num INTEGER," +
-                "state	INTEGER," +
-                "FOREIGN KEY(id) REFERENCES member(id) ON DELETE CASCADE," +
-                "FOREIGN KEY(t_num) REFERENCES team(t_num) ON UPDATE CASCADE" +
+        // contest 테이블
+        db!!.execSQL("CREATE TABLE IF NOT EXISTS contest (" +
+                "c_num INTEGER NOT NULL," +
+                "c_name TEXT NOT NULL," +
+                " c_photo TEXT," +
+                "c_host TEXT NOT NULL," +
+                "c_section TEXT NOT NULL," +
+                "c_start TEXT NOT NULL," +
+                "c_end TEXT NOT NULL," +
+                "c_address TEXT NOT NULL," +
+                "c_detail TEXT," +
+                "PRIMARY KEY(c_num AUTOINCREMENT)" +
                 ");")
 
+        // member 테이블
         db!!.execSQL("CREATE TABLE member (" +
-                "name TEXT NOT NULL," +
-                "id TEXT NOT NULL UNIQUE," +
-                "pw TEXT NOT NULL," +
-                "profile BLOB," +
-                "tel TEXT NOT NULL," +
-                "year TEXT NOT NULL," +
-                "month TEXT NOT NULL," +
-                "date TEXT NOT NULL," +
-                "email TEXT NOT NULL," +
-                "job TEXT NOT NULL," +
-                "univ TEXT," +
-                "area TEXT NOT NULL," +
-                "interest TEXT NOT NULL," +
-                "PRIMARY KEY(id)" +
+                "m_name text NOT NULL," +
+                "m_id TEXT NOT NULL," +
+                "m_pw TEXT NOT NULL," +
+                "m_profile BLOB," +
+                "m_tel TEXT NOT NULL," +
+                "m_year TEXT NOT NULL," +
+                "m_month TEXT NOT NULL," +
+                "m_date TEXT NOT NULL," +
+                "m_email TEXT NOT NULL," +
+                "m_job TEXT NOT NULL," +
+                "m_univ TEXT," +
+                "m_area TEXT NOT NULL," +
+                "m_interest TEXT NOT NULL," +
+                "PRIMARY KEY(m_id)" +
                 ");")
 
+        // resume 테이블
+        db!!.execSQL("CREATE TABLE IF NOT EXISTS resume (" +
+                "r_idx INTEGER NOT NULL," +
+                "m_id TEXT NOT NULL," +
+                "t_num INTEGER NOT NULL," +
+                "r_hope TEXT NOT NULL," +
+                "r_self_intro TEXT NOT NULL," +
+                "r_etc TEXT," +
+                "PRIMARY KEY(r_idx)" +
+                ");")
+
+        // team 테이블
+        db!!.execSQL("CREATE TABLE IF NOT EXISTS team (" +
+                "t_num INTEGER NOT NULL," +
+                "c_num INTEGER NOT NULL," +
+                "t_name TEXT NOT NULL," +
+                "t_host TEXT NOT NULL," +
+                "t_total_num INTEGER NOT NULL," +
+                "t_now_num INTEGER," +
+                "t_end_date TEXT NOT NULL," +
+                "t_need_part TEXT NOT NULL," +
+                "t_detail TEXT NOT NULL," +
+                "PRIMARY KEY(t_num)" +
+                ");")
+
+        // teamManage 테이블
+        db!!.execSQL("CREATE TABLE teamManage (" +
+                "tm_idx INTEGER NOT NULL," +
+                "m_id TEXT NOT NULL," +
+                "t_num INTEGER NOT NULL," +
+                "state INTEGER NOT NULL," +
+                "FOREIGN KEY(m_id) REFERENCES member(m_id) ON DELETE CASCADE," +
+                "FOREIGN KEY(t_num) REFERENCES team(t_num) ON UPDATE CASCADE," +
+                "PRIMARY KEY(tm_idx)" +
+                ");")
+
+        // wishlist 테이블
+        db!!.execSQL("CREATE TABLE wishlist (" +
+                "w_idx INTEGER NOT NULL," +
+                "m_id TEXT NOT NULL," +
+                "c_num INTEGER NOT NULL," +
+                "state INTEGER NOT NULL," +
+                "PRIMARY KEY(w_idx)," +
+                "FOREIGN KEY(m_id) REFERENCES member(m_id) ON DELETE CASCADE," +
+                "FOREIGN KEY(c_num) REFERENCES contest(c_num) ON UPDATE CASCADE" +
+                ");")
     }
 
     override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
